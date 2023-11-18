6214f7caa98751eeba159f629196e383d449184b
GustavoAbranches <gusabranches@gmail.com>
2023-11-18 20:44:13 -0300

    complete exercises1, 2, 3, 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('')
 };
+
+const strInput = 'Hello'
+const stringResult = reverseString(strInput)
+
+console.log(strInput)
 
 // Do not edit below this line
 module.exports = reverseString;
